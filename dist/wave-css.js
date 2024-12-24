1772bd774cc51455764ebd68d525ea8b01174162
Matthew Duffield <matt.duffield@gmail.com>
2024-12-24 15:45:58 -0800

    More work on wc-save-button.

--- dist/wave-css.js
+++ dist/wave-css.js
@@ -2516,7 +2516,6 @@ if (!customElements.get("wc-save-button")) {
         <button type="button" class="btn"
           hx-post="${saveUrl}"
           data-url="${saveUrl}">Save</button>
-        <input type="hidden" name="__redirect-url">
         <div class="dropdown">
           <button type="button" class="btn" style="border-left:1px solid var(--component-border-color);">
             <svg class="h-3 w-3 pointer-events-none"
