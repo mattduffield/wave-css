0ee6bb5dc60babbb4959345724b29cd4e0effe50
Matthew Duffield <matt.duffield@gmail.com>
2025-01-02 09:54:36 -0800

    Added more skeleton components.

--- src/js/components/wc-list-skeleton.js
+++ src/js/components/wc-list-skeleton.js
@@ -44,39 +44,39 @@ if (!customElements.get('wc-list-skeleton')) {
       this.componentElement.className = 'max-w-md p-4 space-y-4 border border-gray-200 divide-y divide-gray-200 rounded-md shadow animate-pulse dark:divide-gray-700 md:p-6 dark:border-gray-700';
       this.componentElement.innerHTML = `
         <div class="flex items-center justify-between">
-          <div>
-            <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5"></div>
-            <div class="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+          <div class="w-5/6">
+            <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-3/6 mb-2.5"></div>
+            <div class="w-5/6 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
           </div>
-          <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
+          <div class="w-1/6 h-2.5 bg-gray-300 rounded-full dark:bg-gray-700"></div>
         </div>
         <div class="flex items-center justify-between pt-4">
-          <div>
-            <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5"></div>
-            <div class="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+          <div class="w-5/6">
+            <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-3/6 mb-2.5"></div>
+            <div class="w-5/6 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
           </div>
-          <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
+          <div class="w-1/6 h-2.5 bg-gray-300 rounded-full dark:bg-gray-700"></div>
         </div>
         <div class="flex items-center justify-between pt-4">
-          <div>
-            <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5"></div>
-            <div class="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+          <div class="w-5/6">
+            <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-3/6 mb-2.5"></div>
+            <div class="w-5/6 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
           </div>
-          <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
+          <div class="w-1/6 h-2.5 bg-gray-300 rounded-full dark:bg-gray-700"></div>
         </div>
         <div class="flex items-center justify-between pt-4">
-          <div>
-            <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5"></div>
-            <div class="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+          <div class="w-5/6">
+            <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-3/6 mb-2.5"></div>
+            <div class="w-5/6 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
           </div>
-          <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
+          <div class="w-1/6 h-2.5 bg-gray-300 rounded-full dark:bg-gray-700"></div>
         </div>
         <div class="flex items-center justify-between pt-4">
-          <div>
-            <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5"></div>
-            <div class="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+          <div class="w-5/6">
+            <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-3/6 mb-2.5"></div>
+            <div class="w-5/6 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
           </div>
-          <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
+          <div class="w-1/6 h-2.5 bg-gray-300 rounded-full dark:bg-gray-700"></div>
         </div>
         <span class="sr-only">Loading...</span>
       `.trim();
