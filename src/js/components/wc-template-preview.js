e6211b6d47980403bc27162dac04ab8f926a302d
Matthew Duffield <matt.duffield@gmail.com>
2025-01-21 17:02:16 -0800

    Added wc-template-preview

--- src/js/components/wc-template-preview.js
+++ src/js/components/wc-template-preview.js
@@ -100,10 +100,10 @@ if (!customElements.get('wc-template-preview')) {
 
       this.componentElement.innerHTML = markup;
 
-      setTimeout(() => {
+      // setTimeout(() => {
         const previewFrame = this.querySelector('iframe.preview');
-        const previewToggle = this.querySelector('wc[name="preview_toggle"]');
-        const dragToggle = this.querySelector('wc[name="drag_toggle"]');
+        const previewToggle = this.querySelector('wc-input[name="preview_toggle"]');
+        const dragToggle = this.querySelector('wc-input[name="drag_toggle"]');
   
         previewToggle.addEventListener('change', (event) => {
           console.log('wc-template-preview:previewToggle change - ', event);
@@ -114,7 +114,7 @@ if (!customElements.get('wc-template-preview')) {
           console.log('wc-template-preview:dragToggle change - ', event);
         });
   
-      }, 250);
+      // }, 250);
     }
 
     _handleAttributeChange(attrName, newValue) {    
