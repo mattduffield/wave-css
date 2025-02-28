3fe90ce01eb943541d356a8c863a7f1fb853fece
Matthew Duffield <matt.duffield@gmail.com>
2025-02-28 09:42:46 -0800

    Working on row edit logic.

--- dist/wave-css.js
+++ dist/wave-css.js
@@ -5244,11 +5244,13 @@ if (!customElements.get("wc-tabulator")) {
         action: (e, row) => {
           const table = row.getTable();
           table.allowEdit = true;
-          const cell = row.getCell("email");
-          cell.edit();
           setTimeout(() => {
-            table.allowEdit = false;
-          }, 500);
+            const cell = row.getCell("email");
+            cell.edit();
+            setTimeout(() => {
+              table.allowEdit = false;
+            }, 500);
+          }, 250);
         }
       },
       {
