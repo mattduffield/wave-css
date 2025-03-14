c9e1835572151549534e4c9dfc3955a4e722c89a
Matthew Duffield <matt.duffield@gmail.com>
2025-03-13 18:09:17 -0700

    More work on getting CodeMirror to support native JavaScript inside web components.

--- dist/wave-css.js
+++ dist/wave-css.js
@@ -2259,7 +2259,7 @@ if (!customElements.get("wc-code-mirror")) {
       existingMarks.forEach((mark) => mark.clear());
       const jsLines = jsContent.split("\n");
       const patterns = [
-        { pattern: /\b(function|var|let|const|return|if|else|for|while|switch|case|break|continue|this|new|typeof|instanceof|class|async|await)\b/g, className: "cm-js-keyword" },
+        { pattern: /\b(function|var|let|const|return|if|else|endif|for|endfor|while|switch|case|break|continue|this|new|typeof|instanceof|class|async|await)\b/g, className: "cm-js-keyword" },
         { pattern: /\b(true|false|null|undefined)\b/g, className: "cm-js-atom" },
         { pattern: /\b\d+(\.\d+)?\b/g, className: "cm-js-number" },
         { pattern: /(["'`])(?:[^\\]|\\.)*?\1/g, className: "cm-js-string" },
